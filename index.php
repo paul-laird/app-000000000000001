53c1cc2299129c496838a7f219f033dfa3b2e99a
paul-laird <35932444+paul-laird@users.noreply.github.com>
2018-01-31 15:51:22 +0000

    Update index.php

--- index.php
+++ index.php
@@ -7,5 +7,6 @@ echo "This file is stored on GitHub.";
 <P> This is <BR/>
   Normal<BR/>
   HTML<BR/>
+  <image src="https://students.dbs.ie/site_images/dbs_logo.png">
   
   <?php echo "And this is from php again." ?>
